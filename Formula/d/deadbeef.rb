3b4a3366e1f23a741f65a2a68c3d8ac0e493a10f
Anirban Dey <code@soufrabi.com>
2023-12-01 18:18:24 +0530

    deadbeef

---
+++ Formula/d/deadbeef.rb
@@ -0,0 +1,18 @@
+class DeaDBeeF < Formula
+  desc "Music Player"
+  homepage "https://github.com/DeaDBeeF-Player/deadbeef"
+  url "https://sourceforge.net/projects/deadbeef/files/travis/linux/1.9.6/deadbeef-static_1.9.6-1_x86_64.tar.bz2/download"
+  version "1.9.6"
+  sha256 "aa17741053f63a7fceace003bf269bd4c4c9e55e42ee14286d9fbf34fbc8e014"
+  license "GPL-2.0"
+
+  def install
+    prefix.install Dir["*"]
+    mkdir_p bin
+    bin.install_symlink "#{prefix}/deadbeef"
+  end
+
+  test do
+    assert_match "DeaDBeeF", shell_output("#{bin}/deadbeef --version 2>/dev/null")
+  end
+end
